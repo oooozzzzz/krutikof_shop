9fe6de47e1ae66d25121e5cfd401ef6f591bf999
oooozzzzz <lobovdima27@gmail.com>
2024-10-07 21:09:46 +0300

    gasd

--- handlers/startHandler.js
+++ handlers/startHandler.js
@@ -4,6 +4,7 @@ const { startMenu } = require("../menus/startMenu");
 
 module.exports = async (ctx) => {
 	await ctx.msg.delete();
+	await ctx.reply("Добро пожаловать!")
 	await ctx.replyWithPhoto("AgACAgIAAxkBAAM6Zvu5ionCDlaj3--pSpshy2GGsOsAAj_lMRu8AAHhS_VnbxKsKJj3AQADAgADcwADNgQ", {reply_markup: startMenu, caption: ctx.t("start")})
 	await createUser(ctx.from.id, ctx.from.first_name);
 };
